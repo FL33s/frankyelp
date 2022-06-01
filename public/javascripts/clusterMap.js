87604779cf97442952966e9fecdd630a3c4b5bf1
FL33s <93101333+FL33s@users.noreply.github.com>
2022-06-01 12:07:44 -0400

    Update clusterMap.js

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,9 +1,12 @@
-mapboxgl.accessToken = mapToken;
+
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmw5NjAiLCJhIjoiY2wzbmR4YnRnMDF4aDNjdDY4NTd0Ynl4MyJ9.Ra-u1Exd6hw5vJ89O7xIaA';
 const map = new mapboxgl.Map({
-    container: 'cluster-map',
-    style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.59179687498357, 40.66995747013945],
-    zoom: 3
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/streets-v11', // style URL
+center: [-74.5, 40], // starting position [lng, lat]
+zoom: 9 // starting zoom
 });
 
 map.addControl(new mapboxgl.NavigationControl());
